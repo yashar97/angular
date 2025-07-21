2b6dfb515be68991053c8b3b53fb0e2963fee08e
yashar <yasharg97@gmail.com>
2025-07-20 21:38:33 -0500

    a

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: false,
+  templateUrl: './register-page.component.html',
+  styles: ``
+})
+export class RegisterPageComponent {
+
+}
---
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-page',
+  standalone: false,
+  templateUrl: './list-page.component.html',
+  styles: ``
+})
+export class ListPageComponent {
+
+}
