09b1f1b197ad7e75f5378ad49bf573b93fe1b800
yashar <yasharg97@gmail.com>
2025-07-23 09:07:38 -0500

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+
+  public heroes: Hero[] = [];
+
+  constructor(private service: HeroesService) { }
+
+  ngOnInit(): void {
+    this.service.getHeroes().subscribe(data => {
+      this.heroes = data;
+    })
+  }
+
+
 
 }
